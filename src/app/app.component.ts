96e9715637019fb07fb3c1651acbd59c634aeceb
Gagan Bahety <gbahety@cimagroup.local>
2020-04-06 10:58:12 +0530

    added error handling and loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  constructor(authService: AuthService) { 
+    authService = authService
+  }
+
 
 }
